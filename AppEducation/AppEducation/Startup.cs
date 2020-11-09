6725b373ec633666f26fc5b8fb102c43abd9c7ca
cuong top <cuongtop4598>
2020-11-09 10:44:44 +0700

    fix

--- AppEducation/AppEducation/Startup.cs
+++ AppEducation/AppEducation/Startup.cs
@@ -27,7 +27,7 @@ namespace AppEducation
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddDistributedMemoryCache();
             services.AddSession(options =>
